5e959b2496d394dcd2f9cb565a13cdd647996992
Sumit630 <135491660+Sumit630@users.noreply.github.com>
2025-02-14 11:05:06 +0530

    second

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1980882709"
+    serviceWorkerVersion: "1500127928"
   }
 });
